8bde98c4891b299f069da4303dc65acb1ec0377d
Breno França <brenofranca@cvccorp.com.br>
2021-08-13 17:33:41 -0300

    First commit

---
+++ back-end/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5350255796f2ffe9a4c94bd09db2cb68'),
+    },
+  },
+});
---
+++ back-end/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'de4a0d1c-4b13-4995-91ad-ffe576251792'
+};
